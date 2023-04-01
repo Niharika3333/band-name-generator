0e4a3f96d2c71223f551289a9139b530bfab433e
niharika33 <63079773+Niharika3333@users.noreply.github.com>
2023-04-01 20:18:24 +0000

    requested GET and POST methods

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-# print("Hello, World!")
-
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "Hello, World!"
+  return render_template('home.html', company_name="Band-Name")
+
+@app.route("/generate", methods=["POST", "GET"])
+def generate():
+  band_name = request.form["city_name"] + request.form["pet_name"]
+  return render_template('band.html', name=band_name )
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
